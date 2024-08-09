2d8ce6cccf7a19b5da1e6a42588808cc86d7438e
kjxbyz <kjxbyz888@outlook.com>
2024-08-09 22:24:54 +0800

    Remove docs

--- docs/constants/index.ts
+++
@@ -1,4 +0,0 @@
-export const cacheLngKey: string = "__flutter-fleet_lng__";
-export const cacheThemeKey: string = "__flutter-fleet_theme__";
-export const basePath =
-  process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? "/flutterfleet" : "";
